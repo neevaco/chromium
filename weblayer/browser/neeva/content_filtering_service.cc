59d97cf3a7ec31a49fc3ef27625270109ff87088
Darin Fisher <darin@neeva.co>
2022-04-21 19:39:57 +0000

    Add some basic test code

--- weblayer/browser/neeva/content_filtering_service.cc
+++ weblayer/browser/neeva/content_filtering_service.cc
@@ -3,9 +3,11 @@
 #include "weblayer/browser/neeva/content_filtering_service.h"
 
 #include "base/logging.h"
+#include "components/url_pattern_index/url_pattern_index.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "mojo/public/cpp/system/buffer.h"
 
 namespace weblayer {
 namespace neeva {
@@ -33,6 +35,34 @@ void ContentFilteringService::Log(const std::string& message) {
 void ContentFilteringService::RefreshRules(
     int64_t current_sequence_num, RefreshRulesCallback callback) {
   // TODO: implement me!
+
+  // XXX test out with some basic data... and validate that we are
+  // getting the right outcome.
+
+  if (current_sequence_num == 0) {
+    flatbuffers::FlatBufferBuilder flat_builder;
+    url_pattern_index::UrlPatternIndexBuilder index_builder(&flat_builder);
+
+    // XXX add rules
+
+    const auto index_offset = index_builder.Finish();
+    flat_builder.Finish(index_offset);
+
+    auto size = flat_builder.GetSize();
+    auto* ptr = flat_builder.GetBufferPointer();
+
+    auto shm = mojo::SharedBufferHandle::Create(size);
+    {
+      mojo::ScopedSharedBufferMapping mapping = shm->Map(size);
+      memcpy(mapping.get(), ptr, size);
+    }
+
+    auto rules = mojom::ContentFilterRules::New();
+    rules->mode = mojom::ContentFilterMode::BLOCK_REQUESTS;
+    rules->url_pattern_data = std::move(shm);
+
+    std::move(callback).Run(1, std::move(rules));
+  }
 }
 
 void ContentFilteringService::OnContentFiltered(
