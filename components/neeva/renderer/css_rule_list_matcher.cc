2745b8b188fe62ce87b31a8215fbce2d11aeac03
Darin Fisher <darin@neeva.co>
2022-08-28 21:34:31 +0000

    Add in generic selectors

--- components/neeva/renderer/css_rule_list_matcher.cc
+++ components/neeva/renderer/css_rule_list_matcher.cc
@@ -8,14 +8,32 @@ namespace neeva {
 
 CssRuleListMatcher::CssRuleListMatcher(const flat::CssRuleList* rule_list)
     : rule_list_(rule_list) {
+  // TODO: Build hashmap to optimize |domain_specific_selectors| lookup.
 }
 
 CssRuleListMatcher::~CssRuleListMatcher() = default;
 
 std::string CssRuleListMatcher::GetStyleSheetForDomain(
     const base::StringPiece& domain) {
-  (void) rule_list_;
-  return std::string();
+  // TODO: Add support for domain specific selectors.
+  // TODO: Add caching? (maybe better done w/ WebString?)
+
+  std::string stylesheet;
+  stylesheet.reserve(4096);  // TODO: Choose more wisely?
+
+  if (rule_list_->generic_selectors()) {
+    for (const auto* selector : *rule_list_->generic_selectors()) {
+      stylesheet.append(selector->str());
+      stylesheet.append(1, ',');
+    }
+  }
+
+  if (!stylesheet.empty()) {
+    stylesheet.erase(stylesheet.end() - 1, stylesheet.end());
+    stylesheet.append("{display:none!important;}");
+  }
+
+  return stylesheet;
 }
 
 }  // namespace neeva
