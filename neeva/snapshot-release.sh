0aee8885ce7577673ebbd5680ed096c49dcc6b0a
Kobe Chang <kobec@neeva.co>
2022-10-18 23:03:53 +0000

    Add rm -rf to the snapshot-release script

--- neeva/snapshot-release.sh
+++ neeva/snapshot-release.sh
@@ -11,7 +11,12 @@ if [ -z "$out" ]; then
 fi
 
 echo "Taking snapshot of $src"
+if [ -e "$out" ]; then 
+  echo -n "$out already exists, hit enter to delete: "
+  read continue_confirmation
+fi
 
+rm -rf $out
 mkdir -p $out
 cp $src/apks/WebLayerSupport.apk $out
 cp $src/apks/WebLayerShell.apk $out
@@ -19,7 +24,7 @@ cp $src/args.gn $out
 
 get_list_of_public_java_sources() {
   # Exclude unneeded browser sandbox code.
-  find org/chromium/weblayer -name \*.java | grep -v BrowserSandboxService.java
+  find org/chromium/weblayer -name \*.java | egrep -v 'BrowserSandboxService.java|BrowserFragmentDelegate.java|BrowserFragmentTabDelegate.java|TabNavigationControllerProxy.java|TabParams.java|TabProxy.java|WebMessageReplyProxyProxy.java'
 }
 
 (cd $src/../../weblayer/public/java && zip -r $out/client-res.zip res)
